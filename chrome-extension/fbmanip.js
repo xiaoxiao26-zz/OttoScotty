609e32cc11fe3d5250709e6959ff038012d48e74
tomtseng <thomasts@andrew.cmu.edu>
2016-02-06 18:27:53 -0500

    Update cdn link for chrome extension

--- chrome-extension/fbmanip.js
+++ chrome-extension/fbmanip.js
@@ -2,6 +2,6 @@
   var newscript = document.createElement('script');
      newscript.type = 'text/javascript';
      newscript.async = true;
-     newscript.src = 'https://cdn.rawgit.com/xiaoxiao26/JavascriptBot/master/JavascriptForwarder.js';
+     newscript.src = 'https://cdn.rawgit.com/xiaoxiao26/OttoScotty/16a810d86f09d9d9f2fecc0388c7dca033c30584/JavascriptForwarder.js';
   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
 })();
