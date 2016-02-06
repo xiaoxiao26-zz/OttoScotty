63beaafa6c86b6506853bfaebc2c3f59e4921ed5
tomtseng <thomasts@andrew.cmu.edu>
2016-02-06 18:17:47 -0500

    https to http in extension src
    fix INSECURE_RESPONSE error

--- chrome-extension/fbmanip.js
+++ chrome-extension/fbmanip.js
@@ -2,6 +2,6 @@
   var newscript = document.createElement('script');
      newscript.type = 'text/javascript';
      newscript.async = true;
-     newscript.src = 'https://www.rawgit.com/xiaoxiao26/JavascriptBot/master/JavascriptForwarder.js';
+     newscript.src = 'http://www.rawgit.com/xiaoxiao26/JavascriptBot/master/JavascriptForwarder.js';
   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
 })();
